ac21ac818c03052a61c857ccdb18a08e84662eb0
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-03-23 15:06:16 -0500

    fix build in certain scenarios

--- generator/build.rs
+++ generator/build.rs
@@ -2,7 +2,7 @@ use std::{fs::File, io, path::Path};
 
 use zip::ZipArchive;
 
-fn unzip_file_to_location(file_path: &str, destination: &str) -> io::Result<()> {
+fn unzip_file_to_location(file_path: &Path, destination: &Path) -> io::Result<()> {
     let file = File::open(file_path)?;
     let mut archive = ZipArchive::new(file)?;
 
@@ -10,7 +10,7 @@ fn unzip_file_to_location(file_path: &str, destination: &str) -> io::Result<()> 
         let mut file = archive.by_index(i)?;
 
         // todo: mangled name good?
-        let outpath = Path::new(destination).join(file.mangled_name());
+        let outpath = destination.join(file.mangled_name());
 
         if (*file.name()).ends_with('/') {
             std::fs::create_dir_all(&outpath)?;
@@ -31,9 +31,14 @@ fn unzip_file_to_location(file_path: &str, destination: &str) -> io::Result<()> 
 fn main() {
     // step 1 unzip generated.zip to OUT_DIR
     let out_dir = std::env::var("OUT_DIR").unwrap();
-    unzip_file_to_location("generated.zip", &out_dir).unwrap();
-
     let out_dir = Path::new(&out_dir);
+
+    let root = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let root = Path::new(&root);
+
+    let generated_zip = root.join("generated.zip");
+
+    unzip_file_to_location(&generated_zip, &out_dir).unwrap();
 
     generator_build::GeneratorConfig {
         input: out_dir.join("generated"),
