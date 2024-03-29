7d26321189238d929230de5b0e4efc8ec8fba491
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-03-29 00:57:07 -0500

    stash

--- quadtree/benches/rebuild.rs
+++ quadtree/benches/rebuild.rs
@@ -1,4 +1,5 @@
 use divan::{AllocProfiler, Bencher};
+use quadtree::rebuild::MoveElement;
 use rand::prelude::SliceRandom;
 
 #[global_allocator]
@@ -10,7 +11,8 @@ fn main() {
 
 // Register a `fibonacci` function and benchmark it over multiple cases.
 
-const LENS: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096];
+//
+const LENS: &[usize] = &[64, 512, 4096, 32768, 262_144];
 
 #[divan::bench(
     args = LENS,
@@ -54,29 +56,66 @@ fn rebuild_every(bencher: Bencher, len: usize) {
     });
 }
 
-// not very helpful and makes results more confusing
-// #[divan::bench(
-// args = LENS,
-// )]
-// fn rebuild_4096(bencher: Bencher, len: usize) {
-//     let v = (0..4096).collect::<Vec<_>>();
-//
-//     let arr = 0..len;
-//
-//     // shuffle the array
-//     let mut arr = arr.collect::<Vec<_>>();
-//     arr.shuffle(&mut rand::thread_rng());
-//
-//     let changes = arr
-//         .iter()
-//         .enumerate()
-//         .map(|(i, &x)| quadtree::rebuild::MoveElement {
-//             remove_from_idx: i,
-//             insert_to_idx: x,
-//         })
-//         .collect::<Vec<_>>();
-//
-//     bencher.counter(4096_usize).bench(|| {
-//         quadtree::rebuild::apply_vec(&v, &changes, &mut [0, 1, 2, 3, 4]);
-//     });
-// }
+#[divan::bench(
+args = LENS,
+)]
+fn rebuild_262144(bencher: Bencher, len: usize) {
+    const LEN: usize = 262_144;
+    let v = (0..LEN).collect::<Vec<_>>();
+
+    let arr = 0..len;
+
+    // shuffle the array
+    let mut arr = arr.collect::<Vec<_>>();
+    arr.shuffle(&mut rand::thread_rng());
+
+    let changes = arr
+        .iter()
+        .enumerate()
+        .map(|(i, &x)| quadtree::rebuild::MoveElement {
+            remove_from_idx: i,
+            insert_to_idx: x,
+        })
+        .collect::<Vec<_>>();
+
+    bencher.bench(|| {
+        quadtree::rebuild::apply_vec(&v, &changes, &mut [0, 1, 2, 3, 4]);
+    });
+}
+
+#[divan::bench(
+args = [64, 512, 4096],
+)]
+fn rebuild_262144_naive(bencher: Bencher, len: usize) {
+    const LEN: usize = 262_144;
+    let v = (0..LEN).collect::<Vec<_>>();
+
+    let arr = 0..len;
+
+    // shuffle the array
+    let mut arr = arr.collect::<Vec<_>>();
+    arr.shuffle(&mut rand::thread_rng());
+
+    let changes = arr
+        .iter()
+        .enumerate()
+        .map(|(i, &x)| quadtree::rebuild::MoveElement {
+            remove_from_idx: i,
+            insert_to_idx: x,
+        })
+        .collect::<Vec<_>>();
+
+    bencher.bench(|| {
+        let mut v = v.clone();
+
+        for &MoveElement {
+            remove_from_idx,
+            insert_to_idx,
+        } in &changes
+        {
+            let elem = v[remove_from_idx];
+            v.remove(remove_from_idx);
+            v.insert(insert_to_idx, elem);
+        }
+    });
+}
