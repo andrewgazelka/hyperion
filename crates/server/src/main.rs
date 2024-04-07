d7776284709525387e5a33ccc40a15cb6e13b0d7
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-04-07 15:34:42 -0500

    perf: disable denormals (#152)

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use server::{adjust_file_limits, Game};
 
 mod tracing_utils;
@@ -10,8 +11,19 @@ fn init() -> anyhow::Result<()> {
     // 10k players, we want at least 2^14 = 16,384 file handles
     adjust_file_limits(16_384)?;
 
+    rayon::ThreadPoolBuilder::new()
+        .spawn_handler(|thread| {
+            std::thread::spawn(|| no_denormals::no_denormals(|| thread.run()));
+            Ok(())
+        })
+        .build_global()
+        .context("failed to build thread pool")?;
+
     let default_address = "0.0.0.0:25565";
-    let mut game = Game::init(default_address)?;
-    game.game_loop();
-    Ok(())
+
+    no_denormals::no_denormals(|| {
+        let mut game = Game::init(default_address)?;
+        game.game_loop();
+        Ok(())
+    })
 }
