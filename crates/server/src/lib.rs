76c79d99eb3a2d5303b9a86099b35de0be264e7f
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-04-10 15:55:24 -0500

    remove uneeded feature

--- crates/server/src/lib.rs
+++ crates/server/src/lib.rs
@@ -1,6 +1,5 @@
 #![feature(thread_local)]
 #![feature(lint_reasons)]
-#![feature(write_all_vectored)]
 #![expect(clippy::type_complexity, reason = "evenio uses a lot of complex types")]
 
 mod chunk;
