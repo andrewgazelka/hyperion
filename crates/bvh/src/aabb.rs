f597399e6207d74000bd295e7f416c87ddeb29c1
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-04-10 21:55:58 -0500

    refactor: remove unecessary unsafe blocks

--- crates/bvh/src/aabb.rs
+++ crates/bvh/src/aabb.rs
@@ -193,42 +193,6 @@ impl Aabb {
         self.max - self.min
     }
 
-    // pub fn containing<T: HasAabb>(input: &[T]) -> Self {
-    //     let mut current_min = Simd::<f32, 4>::splat(f32::INFINITY);
-    //     let mut current_max = Simd::<f32, 4>::splat(f32::NEG_INFINITY);
-    //
-    //     // optimized for neon (128 bit registers)
-    //     // todo: use chunks so we can to support AVX-512 (512 bit registers)
-    //     for elem in input {
-    //         let elem = elem.aabb();
-    //         let min = elem.min.as_ref();
-    //
-    //         // make it min [f32; 4]
-    //         let min_ptr = min.as_ptr();
-    //
-    //         // todo: is this safe?
-    //         let min = unsafe { std::slice::from_raw_parts(min_ptr.sub(1), 4) };
-    //
-    //         let min = Simd::<f32, 4>::from_slice(min);
-    //         current_min = current_min.simd_min(min);
-    //
-    //         let max = elem.max.as_ref();
-    //         let max_ptr = max.as_ptr();
-    //         let max = unsafe { std::slice::from_raw_parts(max_ptr.sub(1), 4) };
-    //
-    //         let max = Simd::<f32, 4>::from_slice(max);
-    //         current_max = current_max.simd_max(max);
-    //     }
-    //
-    //     let current_min = current_min.as_array();
-    //     let current_max = current_max.as_array();
-    //
-    //     let min = glam::Vec3::new(current_min[1], current_min[2], current_min[3]);
-    //     let max = glam::Vec3::new(current_max[1], current_max[2], current_max[3]);
-    //
-    //     Self { min, max }
-    // }
-
     pub fn containing<T: HasAabb>(input: &[T]) -> Self {
         let mut current_min = glam::Vec3::splat(f32::INFINITY);
         let mut current_max = glam::Vec3::splat(f32::NEG_INFINITY);
