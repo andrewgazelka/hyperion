a0b7e92fe6792666b5877d584b18efe085e55319
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-04-13 09:07:42 -0500

    feat: port/ip adjustable via cli

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,6 +1,7 @@
 //! The main entry point for the server.
 
 use anyhow::Context;
+use clap::Parser;
 use server::{adjust_file_limits, Game};
 
 mod tracing_utils;
@@ -9,8 +10,23 @@ fn main() -> anyhow::Result<()> {
     tracing_utils::with_tracing(init)
 }
 
+/// The arguments to run the server
+#[derive(Parser)]
+struct Args {
+    /// The IP address the server should listen on. Defaults to 0.0.0.0
+    #[clap(short, long, default_value = "0.0.0.0")]
+    ip: String,
+    /// The port the server should listen on. Defaults to 25565
+    #[clap(short, long, default_value = "25565")]
+    port: u16,
+}
+
 /// Initializes the server.
 fn init() -> anyhow::Result<()> {
+    let Args { ip, port } = Args::parse();
+
+    let default_address = format!("{ip}:{port}");
+
     // 10k players, we want at least 2^14 = 16,384 file handles
     adjust_file_limits(16_384)?;
 
@@ -21,8 +37,6 @@ fn init() -> anyhow::Result<()> {
         })
         .build_global()
         .context("failed to build thread pool")?;
-
-    let default_address = "0.0.0.0:25565";
 
     no_denormals::no_denormals(|| {
         let mut game = Game::init(default_address)?;
