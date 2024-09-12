e4fab717bea4100645b218b2a8f695f2dbea5c34
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-09-12 09:46:12 -0500

    `/zombie` now turns player into zombie.

--- events/infection/src/command.rs
+++ events/infection/src/command.rs
@@ -1,17 +1,27 @@
+use std::{borrow::Cow, ops::Deref};
+
 use flecs_ecs::core::{EntityViewGet, QueryBuilderImpl, SystemAPI, TermBuilderImpl, World};
 use hyperion::{
-    component::command::{get_root_command, Command, Parser},
+    component::{
+        command::{get_root_command, Command, Parser},
+        InGameName, Uuid,
+    },
     event,
     event::EventQueue,
     net::{Compose, NetworkStreamRef},
+    system::player_join_world::list::{PlayerListActions, PlayerListEntry, PlayerListS2c},
+    uuid, valence_protocol,
     valence_protocol::{
+        game_mode::OptGameMode,
+        ident,
         packets::{
             play,
             play::{player_abilities_s2c::PlayerAbilitiesFlags, PlayerAbilitiesS2c},
         },
         text::IntoText,
+        GameMode, VarInt,
     },
-    SystemRegistry,
+    SystemId, SystemRegistry,
 };
 use tracing::{debug, trace_span};
 
@@ -47,6 +57,16 @@ pub fn add_to_tree(world: &World) {
         .child_of_id(speed);
 }
 
+struct CommandContext<'a> {
+    stream: NetworkStreamRef,
+    team: &'a mut Team,
+    compose: &'a Compose,
+    world: &'a World,
+    system_id: SystemId,
+    uuid: uuid::Uuid,
+    name: &'a InGameName,
+}
+
 pub fn process(world: &World, registry: &mut SystemRegistry) {
     let system_id = registry.register();
 
@@ -75,43 +95,144 @@ pub fn process(world: &World, registry: &mut SystemRegistry) {
 
                 world
                     .entity_from_id(event.by)
-                    .get::<(&NetworkStreamRef, &mut Team)>(|(stream, team)| {
-                        match command {
-                            ParsedCommand::Speed(amount) => {
-                                let msg = format!("Setting speed to {amount}");
-                                let pkt = play::GameMessageS2c {
-                                    chat: msg.into_cow_text(),
-                                    overlay: false,
-                                };
-
-                                compose.unicast(&pkt, *stream, system_id, &world).unwrap();
-
-                                let pkt = fly_speed_packet(amount);
-                                compose.unicast(&pkt, *stream, system_id, &world).unwrap();
-                            }
-                            ParsedCommand::Team => {
-                                let msg = format!("You are now on team {team}");
-                                let text = play::GameMessageS2c {
-                                    chat: msg.into_cow_text(),
-                                    overlay: false,
-                                };
-                                compose.unicast(&text, *stream, system_id, &world).unwrap();
-                            }
-                            ParsedCommand::Zombie => {
-                                let msg = "Turning to zombie";
-
-                                // todo: maybe this should be an event?
-                                let text = play::GameMessageS2c {
-                                    chat: msg.into_cow_text(),
-                                    overlay: false,
-                                };
-                                compose.unicast(&text, *stream, system_id, &world).unwrap();
-                                *team = Team::Zombie;
-                            }
-                        }
-                    });
+                    .get::<(&NetworkStreamRef, &mut Team, &Uuid, &InGameName)>(
+                        |(stream, team, uuid, name)| {
+                            let context = CommandContext {
+                                stream: *stream,
+                                team,
+                                compose,
+                                world: &world,
+                                system_id,
+                                uuid: uuid.0,
+                                name,
+                            };
+                            process_command(command, context);
+                        },
+                    );
             }
         });
+}
+
+fn process_command(command: ParsedCommand, context: CommandContext) {
+    match command {
+        ParsedCommand::Speed(amount) => handle_speed_command(amount, context),
+        ParsedCommand::Team => handle_team_command(context),
+        ParsedCommand::Zombie => handle_zombie_command(context),
+    }
+}
+
+fn handle_speed_command(amount: f32, context: CommandContext) {
+    let msg = format!("Setting speed to {amount}");
+    let pkt = play::GameMessageS2c {
+        chat: msg.into_cow_text(),
+        overlay: false,
+    };
+
+    context
+        .compose
+        .unicast(&pkt, context.stream, context.system_id, context.world)
+        .unwrap();
+
+    let pkt = fly_speed_packet(amount);
+    context
+        .compose
+        .unicast(&pkt, context.stream, context.system_id, context.world)
+        .unwrap();
+}
+
+fn handle_team_command(context: CommandContext) {
+    let msg = format!("You are now on team {}", context.team);
+    let text = play::GameMessageS2c {
+        chat: msg.into_cow_text(),
+        overlay: false,
+    };
+    context
+        .compose
+        .unicast(&text, context.stream, context.system_id, context.world)
+        .unwrap();
+}
+
+fn handle_zombie_command(mut context: CommandContext) {
+    static ZOMBIE_PROPERTY: std::sync::LazyLock<valence_protocol::profile::Property> =
+        std::sync::LazyLock::new(|| {
+            let skin = include_str!("zombie_skin.json");
+            let json: serde_json::Value = serde_json::from_str(skin).unwrap();
+
+            let value = json["textures"].as_str().unwrap().to_string();
+            let signature = json["signature"].as_str().unwrap().to_string();
+
+            valence_protocol::profile::Property {
+                name: "textures".to_string(),
+                value,
+                signature: Some(signature),
+            }
+        });
+
+    let msg = "Turning to zombie";
+
+    // todo: maybe this should be an event?
+    let text = play::GameMessageS2c {
+        chat: msg.into_cow_text(),
+        overlay: false,
+    };
+    context
+        .compose
+        .unicast(&text, context.stream, context.system_id, context.world)
+        .unwrap();
+
+    let uuids = &[context.uuid];
+    // remove from list
+    let pkt = play::PlayerRemoveS2c {
+        uuids: Cow::Borrowed(uuids),
+    };
+
+    context
+        .compose
+        .unicast(&pkt, context.stream, context.system_id, context.world)
+        .unwrap();
+
+    let zombie = &*ZOMBIE_PROPERTY;
+    let property = core::slice::from_ref(zombie);
+
+    let singleton_entry = &[PlayerListEntry {
+        player_uuid: context.uuid,
+        username: Cow::Borrowed(context.name),
+        properties: Cow::Borrowed(property),
+        chat_data: None,
+        listed: true,
+        ping: 20,
+        game_mode: GameMode::Survival,
+        display_name: Some(context.name.to_string().into_cow_text()),
+    }];
+
+    let pkt = PlayerListS2c {
+        actions: PlayerListActions::default().with_add_player(true),
+        entries: Cow::Borrowed(singleton_entry),
+    };
+
+    context
+        .compose
+        .unicast(&pkt, context.stream, context.system_id, context.world)
+        .unwrap();
+
+    // first we need to respawn the player
+    let pkt = play::PlayerRespawnS2c {
+        dimension_type_name: ident!("minecraft:overworld").into(),
+        dimension_name: ident!("minecraft:overworld").into(),
+        hashed_seed: 0,
+        game_mode: GameMode::Adventure,
+        previous_game_mode: OptGameMode::default(),
+        is_debug: false,
+        is_flat: false,
+        copy_metadata: false,
+        last_death_location: None,
+        portal_cooldown: VarInt::default(),
+    };
+
+    context
+        .compose
+        .unicast(&pkt, context.stream, context.system_id, context.world)
+        .unwrap();
 }
 
 fn fly_speed_packet(amount: f32) -> PlayerAbilitiesS2c {
