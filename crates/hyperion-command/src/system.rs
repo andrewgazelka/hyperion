da0634f1f5d31e1415a0a8eb5afab0dade0967a8
e1pupper <tebaremahmed@gmail.com>
2024-12-02 15:04:10 +0100

    Remove Regex

--- crates/hyperion-command/src/system.rs
+++ crates/hyperion-command/src/system.rs
@@ -11,7 +11,6 @@ use hyperion::{
     storage::{EventQueue, GlobalEventHandlers},
     system_registry::SystemId,
 };
-use regex::Regex;
 
 use crate::component::CommandRegistry;
 
@@ -20,8 +19,6 @@ pub struct CommandSystemModule;
 
 impl Module for CommandSystemModule {
     fn module(world: &World) {
-        static COMMAND_REGEX: OnceLock<Regex> = OnceLock::new();
-
         system!(
             "execute_command",
             world,
@@ -69,24 +66,19 @@ impl Module for CommandSystemModule {
             }
         });
 
-        let command_regex = Regex::new(r"/(?P<command>\w+).*").unwrap();
-        COMMAND_REGEX.set(command_regex).unwrap();
-
         world.get::<&mut GlobalEventHandlers>(|handlers| {
             handlers.completion.register(|query, completion| {
                 let input = completion.query;
 
                 // should be in form "/{command}"
-                let command_regex = COMMAND_REGEX.get().unwrap();
-                let Some(captures) = command_regex.captures(input) else {
-                    return;
-                };
+                let command = input.strip_prefix("/").unwrap_or(input)
+                    .split_whitespace()
+                    .next()
+                    .unwrap_or("");
 
-                let Some(command) = captures.name("command") else {
+                if command.is_empty() {
                     return;
-                };
-
-                let command = command.as_str();
+                }
 
                 query.world.get::<&CommandRegistry>(|registry| {
                     let Some(cmd) = registry.commands.get(command) else {
