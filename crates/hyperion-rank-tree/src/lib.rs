3808f74d28f55a2209f3976e17f778f01b682ff1
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-12-03 14:59:08 -0800

    feat: add click event (#632)

--- crates/hyperion-rank-tree/src/lib.rs
+++ crates/hyperion-rank-tree/src/lib.rs
@@ -6,7 +6,7 @@ use flecs_ecs::{
 };
 use hyperion::{
     simulation::Player,
-    storage::{ClickEvent, EventFn},
+    storage::{EventFn, InteractEvent},
 };
 
 pub mod inventory;
@@ -66,7 +66,7 @@ impl Module for RankTree {
             .component::<Player>()
             .add_trait::<(flecs::With, Class)>();
 
-        let handler: EventFn<ClickEvent> = |query, _| {
+        let handler: EventFn<InteractEvent> = |query, _| {
             let cursor = query.inventory.get_cursor();
             tracing::debug!("clicked {cursor:?}");
         };
