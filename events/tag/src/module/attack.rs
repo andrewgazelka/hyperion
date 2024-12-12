555709c76da6463017d7e23a8a6f6c86668fdbd2
GroobleDierne <grooble.dierne@gmail.com>
2024-12-12 03:12:45 +0100

    fix: clippy

--- events/tag/src/module/attack.rs
+++ events/tag/src/module/attack.rs
@@ -162,7 +162,7 @@ impl Module for AttackModule {
                                     }
 
                                     if target_team == origin_team {
-                                        let msg = format!("§cCannot attack teammates");
+                                        let msg = "§cCannot attack teammates";
                                         let pkt_msg = play::GameMessageS2c {
                                             chat: msg.into_cow_text(),
                                             overlay: false,
@@ -452,7 +452,7 @@ impl Module for AttackModule {
                                         // player died, increment kill count
                                         kill_count.kill_count += 1;
 
-                                        target.set::<Team>(origin_team.clone());
+                                        target.set::<Team>(*origin_team);
 
                                         return;
                                     }
